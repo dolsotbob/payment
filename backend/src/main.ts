8bd6e24d7a9a74559581e7e4926650714e3b95dc
dolsotbob <1hyoon3@naver.com>
2025-07-09 16:29:24 +0900

    fix: specified app.enableCors in backend/src/main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,11 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://payment-git-main-dolsotbobs-projects.vercel.app/',
+    credentials: true, // 쿠키 등 자격 정보 포함 여부 (필요 없으면 false)
+  });
+  // Render는 자체 포트를 process.env.PORT로 전달함; .env의 PORT는 주석처리 되어 있음 
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
