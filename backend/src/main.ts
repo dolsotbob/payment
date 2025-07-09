4469b7ad2a8d0389338d34832aabe3f31123ebbf
dolsotbob <1hyoon3@naver.com>
2025-07-09 16:43:29 +0900

    fix: removed / at the end of origin url in app.enableCors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,10 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://payment-git-main-dolsotbobs-projects.vercel.app/',
+    origin: [
+      'http://localhost:3000',
+      'https://payment-git-main-dolsotbobs-projects.vercel.app',
+    ],
     credentials: true, // 쿠키 등 자격 정보 포함 여부 (필요 없으면 false)
   });
   // Render는 자체 포트를 process.env.PORT로 전달함; .env의 PORT는 주석처리 되어 있음 
