08ed849510e9cc7d4ed8b65926c68de93dac52f9
dolsotbob <1hyoon3@naver.com>
2025-07-15 14:12:01 +0900

    added @GET('/healthz') to AppController

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -3,7 +3,12 @@ import { Controller, Get } from '@nestjs/common';
 @Controller()
 export class AppController {
     @Get()
-    healthCheck(): string {
+    root(): string {
         return 'OK'; // Render가 이 응답을 받으면 배포가 완료됩니다.
+    }
+
+    @Get('/healthz')
+    healthCheck(): string {
+        return 'OK';
     }
 }
