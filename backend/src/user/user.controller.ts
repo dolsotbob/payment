e98b4fd04d32617ed34316a88406a3b5aab8b0b8
dolsotbob <1hyoon3@naver.com>
2025-07-30 13:26:18 +0900

    fix(user.controller): rename express Request type to avoid conflic with NestJS @Request decorator and ensure walletAddress access with proper typing

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,17 +1,18 @@
-import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import { Request as ExpressRequest } from 'express';
+import { Controller, Get, UseGuards, Request as Req } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
 
 interface JwtPayload {
-    userId: number;
-    username: string;
+    walletAddress: string;
 }
 
 @Controller('user')
 export class UserController {
     @UseGuards(AuthGuard('jwt'))  // 보호된 API 
     @Get('me')
-    getProfile(@Req() req) {
-        return req.user.walletAddress; // JwtStrategy에서 리턴한 값이 여기에 담김
+    getProfile(@Req() req: ExpressRequest & { user: JwtPayload }) {
+        return {
+            walletAddress: req.user.walletAddress, // JwtStrategy에서 리턴한 값이 여기에 담김
+        };
     }
 }
