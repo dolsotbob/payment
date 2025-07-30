0fd3fb0c295c460e9c17254e9681f56e0a721799
dolsotbob <1hyoon3@naver.com>
2025-07-30 09:36:15 +0900

    fix(login-history.module): import UserModule

--- backend/src/login-history/login-history.module.ts
+++ backend/src/login-history/login-history.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { LoginHistory } from './entities/login-history.entity';
 import { LoginHistoryService } from './login-history.service';
 import { LoginHistoryController } from './login-history.controller';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([LoginHistory])],
+    imports: [
+        TypeOrmModule.forFeature([LoginHistory]),
+        UserModule,
+    ],
     controllers: [LoginHistoryController],
     providers: [LoginHistoryService],
     exports: [LoginHistory],
