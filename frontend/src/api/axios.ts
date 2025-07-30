020c33da764df6852bf51820bf10389635909978
dolsotbob <1hyoon3@naver.com>
2025-07-30 09:57:58 +0900

    chore(axios.ts): enable credential sharing with withCredentials: true

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -5,6 +5,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: `${process.env.REACT_APP_BACKEND_URL}`,
+    withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
