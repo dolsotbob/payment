a89b56f489a8ffa625f99a102600e2f50638c484
dolsotbob <1hyoon3@naver.com>
2025-08-19 13:10:41 +0900

    refactor(backend): add coupons.module and register it in app.module

---
+++ backend/src/coupons/coupons.module.ts
@@ -0,0 +1,19 @@
+// src/coupons/coupons.module.ts
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CouponsController } from './coupons.controller';
+import { CouponsService } from './coupons.service';
+
+// 쿠폰 사용 기록이 결제 도메인에 있다면 필요 엔티티만 이 모듈에서 등록
+import { CouponUse } from '../payment/entities/coupon-use.entity';
+// 다른 엔티티가 필요하면 여기 추가
+
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([CouponUse]),
+    ],
+    controllers: [CouponsController],
+    providers: [CouponsService],
+    exports: [CouponsService], // 다른 모듈에서 쓰면 export
+})
+export class CouponsModule { }
